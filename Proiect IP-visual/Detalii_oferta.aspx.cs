2319525871db504e98919cf1bf61064fc36b2fc0
razvan393 <razvanghinea05@gmail.com>
2015-05-24 15:47:52 +0300

    Recomandari2.0

--- Proiect IP-visual/Detalii_oferta.aspx.cs
+++ Proiect IP-visual/Detalii_oferta.aspx.cs
@@ -25,14 +25,15 @@ public partial class Detalii_oferta : System.Web.UI.Page
             {
                 try
                 {
-                    nume2 = Session["USER_ID"].ToString();
+                    
 
                     string nume = "select UserId from aspnet_Membership where username='" + nume2 + "'";
                     SqlConnection conn = new SqlConnection(ConfigurationManager.ConnectionStrings["ConnectionString"].ConnectionString);
                     conn.Open();
                     SqlCommand cmd6 = new SqlCommand(nume,conn);
-                    object nume3 = cmd6.ExecuteScalar();
-                    nume3 = Convert.ToString(nume3) ;
+                    object name = cmd6.ExecuteScalar();
+                    string nume3 = Convert.ToString(name);
+                    
                     conn.Close();
 
                     
@@ -42,52 +43,82 @@ public partial class Detalii_oferta : System.Web.UI.Page
                     SqlDataSource1.SelectParameters.Add("q", q);
                     SqlDataSource1.DataBind();
 
-                    string stele_sejur = "select Hotel.stele from Hotel, Sejur where Hotel.id_hotel=Sejur.id_hotel and Sejur.id_sejur=@q";
-                    string stele_recomand = "select stele from Recomandari where id_user='" + nume3 + "'";
-                    string vizite = "select nr_vizite from Recomandari where id_user='" + nume3 + "'";
-                    
                     sqlVerif = "Select count(*) from Oferta_vizite where id_oferta=@q";
                     SqlConnection con6 = new SqlConnection(ConfigurationManager.ConnectionStrings["ConnectionString"].ConnectionString);
                     con6.Open();
                     SqlCommand com6 = new SqlCommand(sqlVerif, con6);
                     com6.Parameters.AddWithValue("q", q);
-                    int userCount = (int)com6.ExecuteScalar();                                    
+                    int userCount = (int)com6.ExecuteScalar();
                     con6.Close();
 
-                    SqlConnection con10 = new SqlConnection(ConfigurationManager.ConnectionStrings["ConnectionString"].ConnectionString);
-                    con10.Open();
-                    SqlCommand com10 = new SqlCommand(stele_sejur, con10);
-                    com10.Parameters.AddWithValue("@q", q);
-                    double stele_sej = Convert.ToDouble(com10.ExecuteScalar());
-                    con10.Close();
+                    if (Session["USER_ID"] != null)
+                    {
+                        nume2 = Session["USER_ID"].ToString();
+                        string stele_sejur = "select Hotel.stele from Hotel, Sejur where Hotel.id_hotel=Sejur.id_hotel and Sejur.id_sejur=@q";
+                        string stele_recomand = "select stele from Recomandari where id_user='" + nume3 + "'";
+                        string vizite = "select nr_vizite from Recomandari where id_user='" + nume3 + "'";
+                        string pret_sejur = "select Sejur.pret from Sejur where Sejur.id_sejur=@q";
+                        string sum_min = "select suma_min from Recomandari  where id_user='" + nume3 + "'";
+                        string sum_max = "select suma_max from Recomandari  where id_user='" + nume3 + "'";
 
-                    SqlConnection con11 = new SqlConnection(ConfigurationManager.ConnectionStrings["ConnectionString"].ConnectionString);
-                    con11.Open();
-                    SqlCommand com11 = new SqlCommand(stele_recomand, con11);
-                    double stele_rec = Convert.ToDouble(com11.ExecuteScalar());
-                    con11.Close();
+                        SqlConnection con10 = new SqlConnection(ConfigurationManager.ConnectionStrings["ConnectionString"].ConnectionString);
+                        con10.Open();
+                        SqlCommand com10 = new SqlCommand(stele_sejur, con10);
+                        com10.Parameters.AddWithValue("@q", q);
+                        double stele_sej = Convert.ToDouble(com10.ExecuteScalar());
+                        con10.Close();
 
-                    SqlConnection con12 = new SqlConnection(ConfigurationManager.ConnectionStrings["ConnectionString"].ConnectionString);
-                    con12.Open();
-                    SqlCommand com12 = new SqlCommand(vizite, con12);                    
-                    double nr_vizite = Convert.ToDouble(com12.ExecuteScalar());
-                    con12.Close();
+                        SqlConnection con11 = new SqlConnection(ConfigurationManager.ConnectionStrings["ConnectionString"].ConnectionString);
+                        con11.Open();
+                        SqlCommand com11 = new SqlCommand(stele_recomand, con11);                    
+                        double stele_rec = Convert.ToDouble(com11.ExecuteScalar());
+                        con11.Close();
 
-                    double new_vizit = nr_vizite + 1;
+                        SqlConnection con12 = new SqlConnection(ConfigurationManager.ConnectionStrings["ConnectionString"].ConnectionString);
+                        con12.Open();
+                        SqlCommand com12 = new SqlCommand(vizite, con12);
+                        double nr_vizite = Convert.ToDouble(com12.ExecuteScalar());
+                        con12.Close();
 
-                    double procent = (stele_rec * nr_vizite + stele_sej)/new_vizit;
-                    nr_vizite = new_vizit;
-                    Response.Write(procent);
-                    string update_rec = "update Recomandari set nr_vizite=@nr_vizite , stele=@procent where id_user=@userId";
-                    SqlConnection con8 = new SqlConnection(ConfigurationManager.ConnectionStrings["ConnectionString"].ConnectionString);
-                    con8.Open();
-                    SqlCommand cmd5 = new SqlCommand(update_rec,con8);
-                    cmd5.Parameters.AddWithValue("@procent",procent);
-                    cmd5.Parameters.AddWithValue("@nr_vizite", nr_vizite);
-                    cmd5.Parameters.AddWithValue("@userId", nume3);
-                    cmd5.ExecuteNonQuery();
-                    con8.Close();
+                        SqlConnection con13 = new SqlConnection(ConfigurationManager.ConnectionStrings["ConnectionString"].ConnectionString);
+                        con13.Open();
+                        SqlCommand com13 = new SqlCommand(pret_sejur, con13);
+                        SqlCommand com14 = new SqlCommand(sum_min, con13);
+                        SqlCommand com15 = new SqlCommand(sum_max, con13);
+                        double pret_sej = Convert.ToDouble(com13.ExecuteScalar());
+                        double min = Convert.ToDouble(com14.ExecuteScalar());
+                        double max = Convert.ToDouble(com15.ExecuteScalar());
+                        con13.Close();
 
+                        double new_vizit = nr_vizite + 1;
+
+                        double procent = (stele_rec * nr_vizite + stele_sej) / new_vizit;
+                        nr_vizite = new_vizit;
+
+                        if (pret_sej < min)
+                        {
+                            min = pret_sej;
+                        }
+                        else
+                        {
+                            if (pret_sej > max)
+                            {
+                                max = pret_sej;
+                            }
+                        }
+                        
+                        string update_rec = "update Recomandari set nr_vizite=@nr_vizite , stele=@procent, suma_min=@min,suma_max=@max where id_user=@userId";
+                        SqlConnection con8 = new SqlConnection(ConfigurationManager.ConnectionStrings["ConnectionString"].ConnectionString);
+                        con8.Open();
+                        SqlCommand cmd5 = new SqlCommand(update_rec, con8);
+                        cmd5.Parameters.AddWithValue("@procent", procent);
+                        cmd5.Parameters.AddWithValue("@nr_vizite", nr_vizite);
+                        cmd5.Parameters.AddWithValue("@userId", nume3);
+                        cmd5.Parameters.AddWithValue("@min", min);
+                        cmd5.Parameters.AddWithValue("@max", max);
+                        cmd5.ExecuteNonQuery();
+                        con8.Close();
+                    }
                     if (userCount > 0)
                     {
                         string sql1 = "update Oferta_vizite set nr_vizite=nr_vizite+1 where id_oferta=@q";
@@ -137,11 +168,17 @@ public partial class Detalii_oferta : System.Web.UI.Page
     {
         string q = Request.Params["q"];
         q = Server.UrlDecode(q);
-        string nume = Session["USER_ID"].ToString();
+        string nume4 = Session["USER_ID"].ToString();
         
         int temp = 0;
         int loc_rez = 0;
-
+        string nume5 = "select UserId from aspnet_Membership where username='" + nume4 + "'";
+        SqlConnection conn = new SqlConnection(ConfigurationManager.ConnectionStrings["ConnectionString"].ConnectionString);
+        conn.Open();
+        SqlCommand cmd6 = new SqlCommand(nume5, conn);
+        object nume6 = cmd6.ExecuteScalar();
+        nume6 = Convert.ToString(nume6);
+        conn.Close();
         /*
         Button img = (Button)sender;
         DataListItem item = (DataListItem)img.NamingContainer;    
@@ -155,6 +192,53 @@ public partial class Detalii_oferta : System.Web.UI.Page
         }
         */
         loc_rez = Convert.ToInt32(nr_loc.Text);
+
+        string get_rez = "select medie_rezervari from Recomandari where id_user=@nume";
+        string get_numar = "select nr_rezervari from Recomandari where id_user=@nume";
+        string pret_sejur = "select Sejur.pret from Sejur where Sejur.id_sejur=@q";
+
+        SqlConnection conn3 = new SqlConnection(ConfigurationManager.ConnectionStrings["ConnectionString"].ConnectionString);
+        conn3.Open();
+
+        SqlCommand cmd7 = new SqlCommand(get_rez, conn3);
+        cmd7.Parameters.AddWithValue("@nume",nume6);
+
+        SqlCommand cmd8 = new SqlCommand(get_numar, conn3);
+        cmd8.Parameters.AddWithValue("@nume", nume6);
+
+        double medie = Convert.ToDouble(cmd7.ExecuteScalar());
+        int numar = Convert.ToInt32(cmd8.ExecuteScalar());
+
+        conn3.Close();
+
+        SqlConnection con13 = new SqlConnection(ConfigurationManager.ConnectionStrings["ConnectionString"].ConnectionString);
+        con13.Open();
+        SqlCommand com13 = new SqlCommand(pret_sejur, con13);
+        double pret_sej = Convert.ToDouble(com13.ExecuteScalar());
+        con13.Close();
+
+        if ((medie != 0) || (numar != 0))
+        {
+            medie = (medie * numar + pret_sej) / (numar + 1);
+            numar = numar + 1;
+        }
+        else
+        {
+            medie = pret_sej;
+            numar = 1;
+        }
+
+
+        SqlConnection conn2 = new SqlConnection(ConfigurationManager.ConnectionStrings["ConnectionString"].ConnectionString);
+        conn2.Open();
+        string update2 = "update Recomandari set medie_rezervari=@medie, nr_rezervari=@numar where id_user=@nume";
+        SqlCommand com2 = new SqlCommand(update2, conn2);
+        com2.Parameters.AddWithValue("@medie", medie);
+        com2.Parameters.AddWithValue("@numar", numar);
+        com2.Parameters.AddWithValue("@nume", nume6);
+        com2.ExecuteNonQuery();
+        conn2.Close();
+
         string cmdText4 = "select locuri_disp from Sejur where id_sejur=@q";
         using (SqlConnection con4 = new SqlConnection(ConfigurationManager.ConnectionStrings["ConnectionString"].ConnectionString))
         {
@@ -187,10 +271,10 @@ public partial class Detalii_oferta : System.Web.UI.Page
             com.Parameters.AddWithValue("@q", q);
             com.ExecuteNonQuery();
             con.Close();
-            nume = Session["USER_ID"].ToString();
+            nume4 = Session["USER_ID"].ToString();
             Guid userId = Guid.NewGuid();
 
-            string cmdText5 = "select UserId from aspnet_Membership where username='"+nume+"'";
+            string cmdText5 = "select UserId from aspnet_Membership where username='"+nume4+"'";
             using (SqlConnection con5 = new SqlConnection(ConfigurationManager.ConnectionStrings["ConnectionString"].ConnectionString))
             {
 
--- Proiect IP-visual/Profil_utilizator.aspx.cs
+++ Proiect IP-visual/Profil_utilizator.aspx.cs
@@ -10,6 +10,8 @@ using System.Web.Security;
 
 public partial class Profil_utilizator : System.Web.UI.Page
 {
+    int sum_min ;
+    int sum_max ;
 
     protected void Page_Load(object sender, EventArgs e)
     {
@@ -92,6 +94,7 @@ public partial class Profil_utilizator : System.Web.UI.Page
             con2.Close();
         }
             string temp3 = string.Empty;
+            
         string cmdText3 = "select Suma_sejur from aspnet_Membership where username='" + usernamevechibox.Text + "'";
         using (SqlConnection con3 = new SqlConnection(ConfigurationManager.ConnectionStrings["ConnectionString"].ConnectionString))
         {
@@ -111,23 +114,27 @@ public partial class Profil_utilizator : System.Web.UI.Page
                     if (temp3 == "sub 100 euro")
                     {
                         unu.Checked = true;
+                        
                     }
                     else if (temp3 == "100-200 euro")
                     {
                         doi.Checked = true;
+                       
                     }
                     else if (temp3 == "200-300 euro")
                     {
                         trei.Checked = true;
+                        
                     }
                     else
                     {
                         patru.Checked=true;
+                        
                     }
-
+            
             
             con3.Close();
-
+           
             string temp4 = string.Empty;
         string cmdText4 = "select Nivel_hotel from aspnet_Membership where username='" + usernamevechibox.Text + "'";
         using (SqlConnection con4 = new SqlConnection(ConfigurationManager.ConnectionStrings["ConnectionString"].ConnectionString))
@@ -171,6 +178,7 @@ public partial class Profil_utilizator : System.Web.UI.Page
                }
                 catch (Exception err)
                 {
+                    Response.Write(err);
                 }
             }
             else
@@ -184,151 +192,167 @@ public partial class Profil_utilizator : System.Web.UI.Page
      
     protected void Button2_Click(object sender, EventArgs e)
     {
-        string rd2=null, rd3=null, rd4=null, rd5=null;
-        int test = 0;
-        string nume = usernamevechibox.Text;
-         if (Sportiv.Checked)
+        try
         {
-            rd2 = Sportiv.Text;
-        }
-        else if (Recreere.Checked)
-        {
-            rd2 = Recreere.Text;
-        }
-        else if (Cultural.Checked)
-        {
-            rd2 = Cultural.Text;
-        }
-        else if (Ingrijire_Sanatate.Checked)
-        {
-            rd2 = Ingrijire_Sanatate.Text;
-        }
-        else
-        {
-
-            test += 1;
-        }
-
-        if (scurta.Checked)
-        {
-            rd3 = scurta.Text;
-        }
-        else if (medie.Checked)
-        {
-            rd3 = medie.Text;
-        }
-        else if (lunga.Checked)
-        {
-            rd3 = lunga.Text;
-        }
-        else
-        {
-
-            test += 1;
-        }
-
-        if (unu.Checked)
-        {
-            rd4 = unu.Text;
-        }
-        else if (doi.Checked)
-        {
-            rd4 = doi.Text;
-        }
-        else if (trei.Checked)
-        {
-            rd4 = trei.Text;
-        }
-        else if (patru.Checked)
-        {
-            rd4 = patru.Text;
-        }
-        else
-        {
-
-            test += 1;
-        }
-        if (first.Checked)
-        {
-            rd5 = first.Text;
-        }
-        else if (second.Checked)
-        {
-            rd5 = second.Text;
-        }
-        else if (third.Checked)
-        {
-            rd5 = third.Text;
-        }
-        else if (fourth.Checked)
-        {
-            rd5 = fourth.Text;
-        }
-        else
-        {
-
-            test += 1;
-        }
-
-        if (test == 4)
-        {
-            message.Visible = true;
-            message.InnerHtml = "Completeaza toate campurile";
-            //Response.Write("Completeaza toate campurile");
-        }
-        else if (nume == null)
-        {
-            //Response.Write("Introdu username-ul folosit");
-            message.Visible = true;
-            message.InnerHtml = "Introdu username-ul folosit";
-        }
-        else
-        {
-            string temp = null;
-            string q = Request.Params["q"];
-            q = Server.UrlDecode(q);
-
-            SqlConnection con = new SqlConnection(ConfigurationManager.ConnectionStrings["ConnectionString"].ConnectionString);
-            con.Open();
-
-            string insert = "update aspnet_Membership set Forma_turism=@turism, Durata_sejur=@dsejur, Suma_sejur=@ssejur, Nivel_hotel=@hotel where username='"+usernamevechibox.Text+"'";      
-
-            SqlCommand com = new SqlCommand(insert,con);
-
-            com.Parameters.AddWithValue("@turism", rd2);
-            com.Parameters.AddWithValue("@dsejur", rd3);
-            com.Parameters.AddWithValue("@ssejur", rd4);
-            com.Parameters.AddWithValue("@hotel", rd5);
-
-            com.ExecuteNonQuery();
-
-            string getid = "Select UserId from aspnet_Membership where username ='" + usernamevechibox.Text + "'";
-            using (SqlCommand com2 = new SqlCommand(getid, con))
+            string rd2 = null, rd3 = null, rd4 = null, rd5 = null;
+            int test = 0;
+            string nume = usernamevechibox.Text;
+            if (Sportiv.Checked)
+            {
+                rd2 = Sportiv.Text;
+            }
+            else if (Recreere.Checked)
+            {
+                rd2 = Recreere.Text;
+            }
+            else if (Cultural.Checked)
+            {
+                rd2 = Cultural.Text;
+            }
+            else if (Ingrijire_Sanatate.Checked)
+            {
+                rd2 = Ingrijire_Sanatate.Text;
+            }
+            else
             {
 
-            using (SqlDataReader reader = com2.ExecuteReader())
+                test += 1;
+            }
+
+            if (scurta.Checked)
             {
-                while (reader.Read())
+                rd3 = scurta.Text;
+            }
+            else if (medie.Checked)
+            {
+                rd3 = medie.Text;
+            }
+            else if (lunga.Checked)
+            {
+                rd3 = lunga.Text;
+            }
+            else
+            {
+
+                test += 1;
+            }
+
+            if (unu.Checked)
+            {
+                rd4 = unu.Text;
+                sum_max = 100;
+            }
+            else if (doi.Checked)
+            {
+                rd4 = doi.Text;
+                sum_min = 101;
+                sum_max = 200;
+            }
+            else if (trei.Checked)
+            {
+                rd4 = trei.Text;
+                sum_min = 201;
+                sum_max = 300;
+            }
+            else if (patru.Checked)
+            {
+                rd4 = patru.Text;
+                sum_min = 301;
+                sum_max = 999;
+            }
+            else
+            {
+
+                test += 1;
+            }
+            if (first.Checked)
+            {
+                rd5 = first.Text;
+            }
+            else if (second.Checked)
+            {
+                rd5 = second.Text;
+            }
+            else if (third.Checked)
+            {
+                rd5 = third.Text;
+            }
+            else if (fourth.Checked)
+            {
+                rd5 = fourth.Text;
+            }
+            else
+            {
+
+                test += 1;
+            }
+
+            if (test == 4)
+            {
+                message.Visible = true;
+                message.InnerHtml = "Completeaza toate campurile";
+                //Response.Write("Completeaza toate campurile");
+            }
+            else if (nume == null)
+            {
+                //Response.Write("Introdu username-ul folosit");
+                message.Visible = true;
+                message.InnerHtml = "Introdu username-ul folosit";
+            }
+            else
+            {
+                string temp = null;
+                string q = Request.Params["q"];
+                q = Server.UrlDecode(q);
+
+                SqlConnection con = new SqlConnection(ConfigurationManager.ConnectionStrings["ConnectionString"].ConnectionString);
+                con.Open();
+
+                string insert = "update aspnet_Membership set Forma_turism=@turism, Durata_sejur=@dsejur, Suma_sejur=@ssejur, Nivel_hotel=@hotel where username='" + usernamevechibox.Text + "'";
+
+                SqlCommand com = new SqlCommand(insert, con);
+
+                com.Parameters.AddWithValue("@turism", rd2);
+                com.Parameters.AddWithValue("@dsejur", rd3);
+                com.Parameters.AddWithValue("@ssejur", rd4);
+                com.Parameters.AddWithValue("@hotel", rd5);
+
+                com.ExecuteNonQuery();
+
+                string getid = "Select UserId from aspnet_Membership where username ='" + usernamevechibox.Text + "'";
+                using (SqlCommand com2 = new SqlCommand(getid, con))
                 {
-                     temp = reader.GetValue(0).ToString();
-                    Response.Write(temp);
+
+                    using (SqlDataReader reader = com2.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            temp = reader.GetValue(0).ToString();
+                            Response.Write(temp);
+                        }
+                    }
+
                 }
-            }
+                int unit = 1;
+                string stele_rec = "insert into Recomandari (id_user,stele,nr_vizite,suma_min,suma_max) values (@q,@stele,@nr,@min,@max)";
+                SqlCommand com3 = new SqlCommand(stele_rec, con);
+                com3.Parameters.AddWithValue("@q", temp);
+                com3.Parameters.AddWithValue("@stele", Convert.ToDouble(rd5));
+                com3.Parameters.AddWithValue("@nr", unit);
+                com3.Parameters.AddWithValue("@min", sum_min);
+                com3.Parameters.AddWithValue("@max", sum_max);
+
+                com3.ExecuteNonQuery();
+                con.Close();
+                Response.Redirect("Home.aspx");
+
+
 
             }
-            int unit = 1;
-            string stele_rec = "insert into Recomandari (id_user,stele,nr_vizite) values (@q,@stele,@nr)";
-            SqlCommand com3 = new SqlCommand(stele_rec, con);
-            com3.Parameters.AddWithValue("@q", temp);
-            com3.Parameters.AddWithValue("@stele", Convert.ToDouble(rd5));
-            com3.Parameters.AddWithValue("@nr", unit);
-
-            com3.ExecuteNonQuery();
-            con.Close();
-            Response.Redirect("Home.aspx");
-
-
-            
+        }
+        catch (Exception err)
+        {
+            Response.Write(err);
         }
     }
     protected void username_Click(object sender, EventArgs e)
