7f58c7028c22076f4dca011c1dba739c302e558a
Gavrila Timea <gavrila.timea@gmail.com>
2015-05-25 23:19:04 +0300

    Frontend stuff

--- Proiect IP-visual/Home.aspx.cs
+++ Proiect IP-visual/Home.aspx.cs
@@ -16,6 +16,7 @@ public partial class PaginaTest : System.Web.UI.Page
                 string nume = Session["USER_ID"].ToString();
                 if (nume != null)
                 {
+                    Titlu.Text = "Recomandate pentru tine";
                     SqlDataSource1.SelectCommand = "SELECT TOP 4 Sejur.id_sejur, Sejur.nume, Sejur.pret, Sejur.id_hotel, Sejur.imagine, Hotel.id_hotel AS idHotel, Hotel.nume AS HotelNume, Hotel.stele, Sejur.id_tipoferta, TipOferta.denumire, Sejur.descriere, Sejur.Forma_turism FROM Sejur INNER JOIN Hotel ON Sejur.id_hotel = Hotel.id_hotel INNER JOIN TipOferta ON Sejur.id_tipoferta = TipOferta.id_tipoferta INNER JOIN Facilitati ON Hotel.id_hotel = Facilitati.id_hotel INNER JOIN aspnet_Membership ON Sejur.Forma_turism = aspnet_Membership.Forma_turism WHERE aspnet_Membership.username = @username";
                     SqlDataSource1.SelectParameters.Clear();
                     SqlDataSource1.SelectParameters.Add("username", nume);
